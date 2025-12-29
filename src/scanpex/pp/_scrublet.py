1a5c013d3620844800d19940f6aafb94bd1c374b
yo-aka-gene <yujiokano@keio.jp>
2025-12-29 10:01:19 +0900

    :sparkles: pp

---
+++ src/scanpex/pp/_scrublet.py
@@ -0,0 +1,18 @@
+from typing import Optional
+import anndata as ad
+import scanpy as sc
+
+
+def scrublet(
+    adata: ad.AnnData,
+    remove: bool = False,
+    update: bool = False,
+    **kwargs
+)-> Optional[ad.AnnData]:
+    if ("predicted_doublet" not in adata.obs.columns) or update:
+        sc.pp.scrublet(adata, **kwargs)
+
+    if remove:
+        return adata[~adata.obs["predicted_doublet"], :].copy()
+
+    return None
