0d1bb5da536823980ddf4c11b8d894dd1ce97780
yo-aka-gene <yujiokano@keio.jp>
2025-12-29 11:34:50 +0900

    :recycle::wrench: add __version__

--- src/scanpex/__init__.py
+++ src/scanpex/__init__.py
@@ -1,3 +1,10 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("scanpex")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 from . import ft, ml, pl, pp, sq, tl
 
 __all__ = [ft, ml, pl, pp, sq, tl]
