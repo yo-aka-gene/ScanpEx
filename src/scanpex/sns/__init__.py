8828f3dc9d1394f9ab2d4bfa74d1cc3f61a9b73c
Yuji Okano <yujiokano@keio.jp>
2026-09-10 16:48:45 +0900

    :sparkles: add sns.violinstrip

--- src/scanpex/sns/__init__.py
+++ src/scanpex/sns/__init__.py
@@ -1,9 +1,11 @@
 from ._catlollipop import catlollipop
 from ._lollipop import lollipop
 from ._vinswarm import vinswarm
+from ._violinstrip import violinstrip
 
 __all__ = [
     "catlollipop",
     "lollipop",
     "vinswarm",
+    "violinstrip",
 ]
